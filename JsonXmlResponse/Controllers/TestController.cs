f28bf0ca5a4baad32f29aadcec486a9ab8b193c1
Lokendra Kumar <Lokendra_Kumar@Dellteam.com>
2020-09-22 00:03:24 +0530

    Code

---
+++ JsonXmlResponse/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonXmlResponse.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("get.{format}"), FormatFilter]
+        public IEnumerable<Employee> Get()
+        {
+            List<Employee> emp = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "Lokendra", Department = "IT" },
+                new Employee { Id = 2, Name = "Ram", Department = "Sales" }
+            };
+            return emp.ToList();
+        }
+
+        [HttpGet("getemp/{id}.{format?}"), FormatFilter]
+        public IEnumerable<Employee> GetEmployee(int id)
+        {
+            List<Employee> emp = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "Lokendra", Department = "IT" },
+                new Employee { Id = 2, Name = "Ram", Department = "Sales" }
+            };
+            return emp.Where(e => e.Id == id).ToList();
+        }
+    }
+}
